e88340f417ac200f7ae0b5709d4380e0b2990696
spettenn <69841778+spettenn@users.noreply.github.com>
2022-11-07 17:26:22 +0100

    please work

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import FunToPlay from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<FunToPlay />);
+  const linkElement = screen.getByText(/learn/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/module/products.ts
+++ src/module/products.ts
@@ -1,14 +1,42 @@
-type Product = {
-    name: string,
-    price: number, 
-    description: string,
-  };
+interface product {
+  name: string,
+  price: number,
+  description: string,
+  title: string,
+  longDescription: string,
+  image: string,
+  category: string,
+  id: number,
+}
+let productOne: product = {
+  name: 'Apple',
+  price: 1.99,
+  description: 'A red apple',
+  title: 'Apple',
+  longDescription: 'A red apple',
+  image: 'https://images.unsplash.com/photo-1558987905-8b8b5d1b3f0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+  category: 'fruit',
+  id: 1,
+}
+let productTwo: product = {
+  name: 'Banana',
+  price: 1.99,
+  description: 'A yellow banana',
+  title: 'Banana',
+  longDescription: 'A yellow banana',
+  image: 'https://images.unsplash.com/photo-1558987905-8b8b5d1b3f0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+  category: 'fruit',
+  id: 2,
+}
+let productThree: product = {
+  name: 'Orange',
+  price: 1.99,
+  description: 'A orange orange',
+  title: 'Orange',
+  longDescription: 'A orange orange',
+  image: 'https://images.unsplash.com/photo-1558987905-8b8b5d1b3f0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+  category: 'fruit',
+  id: 3,
+}
 
-let products: Product =
-{
-    name: 'Apple',
-    price: 1.99,
-    description: 'A red apple',
-};
-
-export default products;
+export default [productOne, productTwo, productThree];
