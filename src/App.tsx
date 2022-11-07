6e489b8ad430e6cff7a53bf745f71f995a397de3
spettenn <69841778+spettenn@users.noreply.github.com>
2022-11-07 22:07:23 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import useLocalStorage from 'use-local-storage';
+
+
 
 function FunToPlay() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
-    <div>
-      Learn
+    <div className="App" data-theme={theme} >
+
+      <h1>Learn</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo maxime eos delectus nulla cum earum beatae, veritatis numquam alias odit perspiciatis. Ducimus facere delectus soluta! Repellat, hic veritatis! Magni, asperiores!</p>
+      <button onClick={toggleTheme}>Toggle {theme === 'light' ? 'dark' : 'light'} Theme</button>
     </div>
   );
 }
---
+++ src/components/Nav/Logo.ts
@@ -0,0 +1,14 @@
+/* import react from 'react';
+import logoDark from '../../assets/logo-dark.png';
+import logoLight from '../../assets/logo-light.png';
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+    return
+    (
+       
+    );
+};
+ */ 
+const Logo = "hello";   
+export default Logo;
